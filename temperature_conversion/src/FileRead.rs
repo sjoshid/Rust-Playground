c738b8b99ed396730efdaacabd06e76792697a1b
Sugs <joshi.sujit020386@gmail.com>
2018-11-15 20:54:26 -0500

    Fixing typo. Because I hate them.

--- temperature_conversion/src/FileRead.rs
+++ temperature_conversion/src/FileRead.rs
@@ -9,14 +9,14 @@ fn main() {
     //they are doing exactly what im trying to do
     //https://github.com/uutils/coreutils/blob/master/src/tail/tail.rs
 
-    let mut current_stop_postion = 10;
+    let mut new_current_position = 10;
 
     while true {
         let mut f = File::open("/home/sugs/Documents/xi-gtk/TailLogTest").expect("file not found");
         let end_position = f.seek(SeekFrom::End(0)).unwrap();
 
-        if(end_position > current_stop_postion) {
-            let diff = end_position - current_stop_postion;
+        if(end_position > new_current_position) {
+            let diff = end_position - new_current_position;
             println!("Diff between End position {} and current_stop_postion is {:?}", end_position, diff);
 
             let mut buf = vec![0; diff as usize];
@@ -24,8 +24,8 @@ fn main() {
             f.read_exact(&mut buf).unwrap();
             println!("{} {:?}", pos, String::from_utf8(buf).unwrap());
 
-            current_stop_postion = end_position;
-            println!("current_stop_postion at end is {}", current_stop_postion);
+            new_current_position = end_position;
+            println!("new_current_position at end is {}", new_current_position);
 
             let one_sec = time::Duration::from_millis(1000);
             thread::sleep(one_sec);
